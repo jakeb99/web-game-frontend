d441372622abf0716469ac1ef96cac9b0005bd99
Jake B <jbuntin4@uwo.ca>
2025-07-28 14:17:02 -0700

    init commit

--- src/App.js
+++ src/App.js
@@ -1,22 +1,47 @@
-import logo from './logo.svg';
+import { useEffect, useState } from 'react';
 import './App.css';
 
 function App() {
+  const [points, setPoints] = useState(0);
+
+  useEffect(() => {
+    const handleMessage = (event) => {
+      console.log(event.data);
+      if (event.data.type === 'UPDATE_POINTS') {
+        setPoints(event.data.points);
+      }
+    };
+    window.addEventListener('message', handleMessage);
+
+    return () => {
+      window.removeEventListener('message', handleMessage);
+    };
+  }, []);
+
+  const sendMessageToUnity = (value) => {
+    const iframe = document.querySelector('iframe');
+    if (iframe) {
+      iframe.contentWindow.postMessage({type: 'CALL_UNITY_FUNCTION', value: value}, "*");
+    }
+  };
+
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <iframe
+        src='https://jakeb99.github.io/web-game/'
+        title='Unity webGL Game'
+        width="1200"
+        height="800"
+        style={{border: 'none'}}
+        onLoad={
+          () => {
+            console.log("Unity WebGL game loaded")
+          }
+        }
+        ></iframe>
+        <p>Points: {points} </p>
+        <button onClick={() => sendMessageToUnity(5)} >Call Unity Function</button>
       </header>
     </div>
   );
